c26df2e00b71dd04e682be50e3965e7069f02531
Tech UO Space <techsupport@uospace.co>
2025-08-29 16:05:57 +0700

    test

--- test-2.php
+++ test-2.php
@@ -1,6 +1,52 @@
 <?php
 
 /**
+ * Memisahkan string "nama-job,nama-job" menjadi array.
+ * @param string $str
+ * @return array
+ */
+function splitJobCharacters($str) {
+    return array_map(function($item) {
+        return explode('-', $item);
+    }, explode(',', $str));
+}
+
+/**
+ * Membalikkan string job (posisi ganjil: 1, 3, 5, ...)
+ * @param array $arr
+ * @return array
+ */
+function reverseJobCharacters($arr) {
+    return array_map(function($item) {
+        if (isset($item[1])) {
+            $item[1] = strrev($item[1]);
+        }
+        return $item;
+    }, $arr);
+}
+
+/**
+ * Mendekripsi setiap huruf job ke huruf sebelumnya (a->z, b->a, dst)
+ * @param array $arr
+ * @return array
+ */
+function decryptJobCharacters($arr) {
+    // (ex: i -> h, a -> z, d -> c, o -> n, s -> r, t -> s, z -> y)
+    return $arr;
+}
+
+/**
+ * Mengelompokkan data menjadi array 2 dimensi: [[nama, job], ...]
+ * @param array $arr
+ * @return array
+ */
+function makingDreamTeam($arr) {
+    return array_map(function($item) {
+        return [$item[0], $item[1]];
+    }, $arr);
+}
+
+/**
  * Fungsi utama yang menggabungkan semua proses.
  * @param string $str
  * @return string
@@ -39,71 +85,3 @@ echo startUpMatchMaking('samir-sfmutvi,basil-sfutqji,eko-sfmutvi') . "<br>";
 
 echo startUpMatchMaking('samir-sfmutvi,basil-sfutqji') . "<br>";
 // Minimum 3 members in the team
-
-
-
-/**
- * Memisahkan string "nama-job,nama-job" menjadi array.
- * @param string $str
- * @return array
- */
-function splitJobCharacters($str) {
-    return array_map(function($item) {
-        return explode('-', $item);
-    }, explode(',', $str));
-}
-
-/**
- * Membalikkan string job (posisi ganjil: 1, 3, 5, ...)
- * @param array $arr
- * @return array
- */
-function reverseJobCharacters($arr) {
-    return array_map(function($item) {
-        if (isset($item[1])) {
-            $item[1] = strrev($item[1]);
-        }
-        return $item;
-    }, $arr);
-}
-
-/**
- * Mendekripsi setiap huruf job ke huruf sebelumnya (a->z, b->a, dst)
- * @param array $arr
- * @return array
- */
-function decryptJobCharacters($arr) {
-    // (ex: i -> h, a -> z, d -> c, o -> n, s -> r, t -> s, z -> y)
-    return array_map(function($item) {
-        if (isset($item[1])) {
-            $item[1] = implode('', array_map(function($char) {
-                // Buat manual
-
-                if ($char === 'a') return 'z';
-                if ($char === 'A') return 'Z';
-                
-                // Tanpa gunakan function ctype
-                // Tanpa gunakan chr dan ord
-                if ($char >= 'a' && $char <= 'z') {
-                    return $char === 'a' ? 'z' : $char - 1;
-                }
-                if ($char >= 'A' && $char <= 'Z') {
-                    return $char === 'A' ? 'Z' : $char - 1;
-                }
-                return $char;
-            }, str_split($item[1])));
-        }
-        return $item;
-    }, $arr);
-}
-
-/**
- * Mengelompokkan data menjadi array 2 dimensi: [[nama, job], ...]
- * @param array $arr
- * @return array
- */
-function makingDreamTeam($arr) {
-    return array_map(function($item) {
-        return [$item[0], $item[1]];
-    }, $arr);
-}
